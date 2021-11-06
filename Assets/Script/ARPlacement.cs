b49cdb7b80bcde0c15f9f3bb14baff8f18a454a5
RafaelM <rafaelm.science@gmail.com>
2021-11-06 18:58:47 -0400

    feat: Scale using pinch touch screen

--- Assets/Script/ARPlacement.cs
+++ Assets/Script/ARPlacement.cs
@@ -13,6 +13,9 @@ public class ARPlacement : MonoBehaviour
     private Pose PlacementePose;
     private ARRaycastManager aRRaycastManager;
     private bool placementPoseIsValid = false;
+    private float initialDistance;
+    private Vector3 initialScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +27,44 @@ public class ARPlacement : MonoBehaviour
     {
         if (spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            ARPlaceObject();
+            ARPlaceObject(); // at the moment this just spawns the gameobject
+        }
+
+        //scale using pinch involves two touches
+        // we need to count both the touches, store it somewhere, measure the distance between pinch
+        // and scale gameobject depending on the pinc distance
+        // we also need to ignore if the pinch distance is smal (case where tow touches are registered accidently)
+
+        if (Input.touchCount == 2)
+        {
+            var touchZero = Input.GetTouch(0);
+            var touchOne = Input.GetTouch(1);
+
+            //if any of touchezero or toucheOne is cancelled or maybe ended then do nothing
+            if (touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled ||
+                touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled)
+            {
+                return; //basically do nothing
+            }
+
+            if (touchZero.phase == TouchPhase.Began || touchOne.phase == TouchPhase.Began)
+            {
+                initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                initialScale = spawnedObject.transform.localScale;
+                Debug.Log("Initial Distance: " + initialDistance + "GameObject Name: " + arObjectToSpawn.name);
+            }
+            else //if touch is moved
+            {
+                var currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
+
+                if (Mathf.Approximately(initialDistance, 0))
+                {
+                    return; // do nothing if it can be ignored where initial distance is very close to zero
+                }
+
+                var factor = currentDistance / initialDistance;
+                spawnedObject.transform.localScale = initialScale * factor;
+            }
         }
 
         UpdatePlacementPose();
